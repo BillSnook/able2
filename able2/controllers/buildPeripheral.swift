3188af0f7ffed538acee286fc8c32449994b7ea6
Bill Snook <bluegoose1@mac.com>
2016-06-11 10:26:45 -0700

    Moved serviceCell to buildPeripheral, almost working, advertise is enabled when uuid field is blank

--- able2/controllers/buildPeripheral.swift
+++ able2/controllers/buildPeripheral.swift
@@ -15,23 +15,33 @@ protocol CellStateChangeProtocol {
 }
 
 
-class buildPeripheral: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CellStateChangeProtocol {
+class buildPeripheral: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITextFieldDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var advertiseButton: UIButton!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
-    var advertising = false
-    
     var builder: Builder?
-    
     var service: Service?
     var characteristics: [Characteristic]?
     
-    var savedState = false
+    var advertising = false
+    
+    
+    @IBOutlet weak var serviceNameField: UITextField!
+    @IBOutlet weak var uuidField: UITextField!
+    @IBOutlet weak var primarySwitch: UISwitch!
+    @IBOutlet weak var uuidButton: UIButton!
+    
+    var displayState = DisplayState.Neutral
+    var nameFieldValid = false
+    var uuidFieldValid = false
+    
     
 //--    ----    ----    ----
     
+    // MARK: - Lifecycle events
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -43,16 +53,14 @@ class buildPeripheral: UIViewController, UICollectionViewDelegate, UICollectionV
         advertiseButton.setTitle( "Advertise", forState: .Normal )
         advertiseButton.setTitleColor( UIColor.blackColor(), forState: .Normal )
         advertiseButton.setTitleColor( UIColor.lightGrayColor(), forState: .Disabled )
-        if service != nil {
-            savedState = true
-            advertiseButton.enabled = true
-        } else {
-            advertiseButton.enabled = false
-        }
+        
+        let serviceValid = service != nil
+        advertiseButton.enabled = serviceValid
+        nameFieldValid = serviceValid
+        uuidFieldValid = serviceValid
+        
         saveButton.enabled = false
         builder?.setupFromService( service )
-//        service = builder?.service
-//        characteristics = builder?.characteristics
 
     }
 
@@ -62,6 +70,21 @@ class buildPeripheral: UIViewController, UICollectionViewDelegate, UICollectionV
         
         service = builder?.service
         characteristics = builder?.characteristics
+
+        serviceNameField.text = service!.name
+        
+        uuidField.text = service!.uuid
+        uuidField.inputView = UIView.init( frame: CGRectZero );    // No keyboard
+        
+        if let primary = service!.primary {
+            primarySwitch.on = primary.boolValue
+        } else {
+            primarySwitch.on = false
+        }
+        
+        textFieldBorderSetup(serviceNameField)
+        textFieldBorderSetup(uuidField)
+
     }
     
     override func viewDidDisappear(animated: Bool) {
@@ -76,56 +99,80 @@ class buildPeripheral: UIViewController, UICollectionViewDelegate, UICollectionV
                                            withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
         // Code here will execute before the rotation begins.
-        // Equivalent to placing it in the deprecated method -[willRotateToInterfaceOrientation:duration:]
         coordinator.animateAlongsideTransition({ (context) -> Void in
             // Place code here to perform animations during the rotation.
             // You can pass nil for this closure if not necessary.
             },
            completion: { (context) -> Void in
             // Code here will execute after the rotation has finished.
-            // Equivalent to placing it in the deprecated method -[didRotateFromInterfaceOrientation:]
             self.collectionView.reloadData()
         })
     }
+    
+    
+    // MARK: - Control actions
 
     @IBAction func saveAction(sender: AnyObject) {
 
         guard service != nil else { print( "save failed" ); return }
         saveButton.enabled = false
         // Gather and save data from fields and create service
-        let indexPath = NSIndexPath(forItem: 0, inSection: 0 )
-        let cell = collectionView.cellForItemAtIndexPath( indexPath ) as! ServicesCollectionViewCell
-        service!.name = cell.serviceNameField.text
-        service!.uuid = cell.uuidField.text
-        service!.primary = NSNumber( bool: cell.primarySwitch.on )
+        service!.name = serviceNameField.text
+        service!.uuid = uuidField.text
+        service!.primary = NSNumber( bool: primarySwitch.on )
         builder?.save()
         advertiseButton.enabled = true
     }
 
-    @IBAction func advertiseChange(sender: AnyObject) {
+    @IBAction func advertiseAction(sender: AnyObject) {
         
+        guard validateService() else { return }
         let adButton = sender as! UIButton
-        let indexPath = NSIndexPath(forItem: adButton.tag, inSection: 0 )
-        let cell = collectionView.cellForItemAtIndexPath( indexPath ) as! ServicesCollectionViewCell
         if !advertising {           // If we were not advertising, now we want to start
-			if cell.cellIsValid() {
-				cell.setStateEnabled( false )
-                adButton.setTitle( "Stop Advertising", forState: .Normal )
-                startAdvertising()
-            } else {
-                // Say why we failed - border colors are handled in cell
-            }
+            guard !saveButton.enabled else { return }   // But must be saved first - may need alert
+            setControlsEnabled( false )
+            adButton.setTitle( "Stop Advertising", forState: .Normal )
+            startAdvertising()
         } else {
             adButton.setTitle( "Advertise", forState: .Normal )
-			cell.setStateEnabled( true )
+			setControlsEnabled( true )
             stopAdvertising()
         }
     }
     
-    @IBAction func addCharacteristicAction(sender: UIButton) {
+    @IBAction func characteristicAction(sender: UIButton) {
         
-        print( "addCharacteristicAction" )
+        print( "characteristicAction" )
+        serviceModified()
     }
+
+    @IBAction func primaryAction(sender: AnyObject) {
+        
+        serviceModified()
+    }
+    
+    @IBAction func makeNewUUIDAction(sender: AnyObject) {
+        
+        let uuid = NSUUID.init()
+        uuidField.text = uuid.UUIDString
+        uuidField.enabled = true    // Allows selection
+        uuidFieldValid = true
+        textFieldBorderSetup( uuidField )
+        serviceModified()
+        
+    }
+    
+    // MARK: - State methods
+    
+    func setControlsEnabled( enabled: (Bool) ) {
+        
+        serviceNameField.enabled = enabled
+        uuidField.enabled = enabled
+        uuidButton.enabled = enabled
+        primarySwitch.enabled = enabled
+        
+    }
+    
 
     // MARK: - Advertising support
     
@@ -139,86 +186,136 @@ class buildPeripheral: UIViewController, UICollectionViewDelegate, UICollectionV
         advertising = false
     }
     
-    // MARK: - CellStateChangeProtocol support
 
-    func stateDidChange() {
+    func serviceModified( nameValid: Bool = true ) {
         
-        print( "buildPeripheral stateDidChange" )
-        savedState = false
+        print( "buildPeripheral serviceModified" )
         
-        let indexPath = NSIndexPath(forItem: 0, inSection: 0 )
-        let cell = collectionView.cellForItemAtIndexPath( indexPath ) as! ServicesCollectionViewCell
-        saveButton.enabled = cell.cellIsValid()
-        advertiseButton.enabled = false
+        let validToSave = nameValid && uuidFieldValid
+        saveButton.enabled = validToSave
+        advertiseButton.enabled = !validToSave
+    }
+    
+    func validateService() -> Bool {    // All text fields have text in them
+        
+        guard nameFieldValid else { return false }
+        guard uuidFieldValid else { return false }
+        return true
     }
 
+    func setBorderOf( textField: (UITextField), toDisplayState: (DisplayState) ) {
+
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 6.0
+        switch toDisplayState {
+        case .Neutral:
+            textField.layer.borderColor = UIColor.lightGrayColor().CGColor
+        case .Valid:
+            textField.layer.borderColor = UIColor.greenColor().CGColor
+        case .Invalid:
+            textField.layer.borderColor = UIColor.redColor().CGColor
+        }
+        
+    }
+    
+    // Verify data is valid for advertising
+    func textFieldNotEmpty( textField: (UITextField) ) -> Bool {
+        
+        if let text = textField.text {
+            if text.isEmpty {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldBorderSetup( textField: (UITextField) ) {
+        
+        if let text = textField.text {
+            if text.isEmpty {
+                setBorderOf( textField, toDisplayState: .Invalid )
+            } else {
+                setBorderOf( textField, toDisplayState: .Valid )
+            }
+        } else {
+            setBorderOf( textField, toDisplayState: .Neutral )
+        }
+    }
+    
+
+    // MARK: - UITextFieldDelegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == uuidField {
+            return false	// false because uuidField should never allow changes to its text
+        }
+        
+        nameFieldValid = false       // Set if cell will be
+        var displayState = DisplayState.Invalid // .Neutral
+        if let text = textField.text {
+//            print( "\ntext: \(text), length: \(text.characters.count)" )
+//            print( "range location: \(range.location), length: \(range.length)" )
+//            print( "string: \(string), length: \(string.characters.count)" )
+            let nonEmptyText = !text.isEmpty && ( range.length != text.characters.count )
+            let nonEmptyReplacement = !string.isEmpty
+            if nonEmptyReplacement || nonEmptyText {
+                nameFieldValid = true
+                displayState = .Valid
+            } else {
+                displayState = .Invalid
+            }
+        }
+        setBorderOf( textField, toDisplayState: displayState )
+        serviceModified( nameFieldValid )
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: (UITextField)) {
+        
+        textFieldBorderSetup( textField )
+    }
+    
     // MARK: - Collection View
 
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         
+        return 0
         guard service != nil else { return 0 }
         return 1
     }
     
     func collectionView( collectionView: UICollectionView, numberOfItemsInSection: NSInteger ) -> NSInteger {
     
-        return 2
         if let count = characteristics?.count {
-            return 1 + count
+            return count
         }
-        return 1
+        return 0
     }
     
     func collectionView( collectionView: UICollectionView,
                           cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        if indexPath.row == 0 { // For advertised service setup
-            let cell = collectionView.dequeueReusableCellWithReuseIdentifier( "ServiceView", forIndexPath: indexPath ) as! ServicesCollectionViewCell
+
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier( "CharacteristicView", forIndexPath: indexPath ) as! CharacteristicsCollectionViewCell
             
-            cell.serviceNameField.text = service!.name
-			cell.serviceNameField.delegate = cell
-			
-            cell.uuidField.text = service!.uuid
-            cell.uuidField.inputView = UIView.init( frame: CGRectZero );    // No keyboard
-			cell.uuidField.delegate = cell
-            cell.tag = indexPath.item
+        cell.uuidField.text = ""
+        cell.uuidField.inputView = UIView.init( frame: CGRectZero );    // No keyboard
+        cell.tag = indexPath.item
 
-            if let primary = service!.primary {
-                cell.primarySwitch.on = primary.boolValue
-            } else {
-                cell.primarySwitch.on = false
-            }
+        cell.valueTextView.text = ""
+        cell.tag = indexPath.item
 
-            cell.textFieldBorderSetup(cell.serviceNameField)
-            cell.textFieldBorderSetup(cell.uuidField)
-            cell.delegate = self
-            return cell
-        } else {
-            let cell = collectionView.dequeueReusableCellWithReuseIdentifier( "CharacteristicView", forIndexPath: indexPath ) as! CharacteristicsCollectionViewCell
-            
-            cell.uuidField.text = ""
-            cell.uuidField.inputView = UIView.init( frame: CGRectZero );    // No keyboard
-            cell.tag = indexPath.item
-
-            cell.valueTextView.text = ""
-            cell.tag = indexPath.item
-
-            cell.textFieldBorderSetup(cell.uuidField)
-            cell.delegate = self
-            return cell
-        }
+        cell.textFieldBorderSetup(cell.uuidField)
+//        cell.delegate = self
+        return cell
     }
     
     func collectionView(collectionView: UICollectionView, layout: UICollectionViewLayout, sizeForItemAtIndexPath: NSIndexPath) -> CGSize {
         
-		if sizeForItemAtIndexPath.row == 0 { // For advertised service setup
-			return CGSizeMake( collectionView.frame.size.width, 100 )
-		} else {
-			if sizeForItemAtIndexPath.row >= 1 { // For characteristic setup
-				return CGSizeMake( collectionView.frame.size.width, 425 )
-			} else {
-				return CGSizeZero
-			}
-		}
+		return CGSizeMake( collectionView.frame.size.width, 425 )
     }
 
 }
